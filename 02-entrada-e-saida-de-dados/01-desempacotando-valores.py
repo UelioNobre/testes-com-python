bb884a099c18ecf70c11d12ebfade5239008d435
uelionobre <uelionobre@hotmail.com>
2023-11-13 12:08:31 -0300

    Desempacotando valores com Python

---
+++ 02-entrada-e-saida-de-dados/01-desempacotando-valores.py
@@ -0,0 +1,15 @@
+print("Desempacotando valores com Python")
+string_original = "cd"
+a, b = string_original
+print(f"String original: {string_original}")
+print(f"O valor da variável a: {a}")
+print(f"O valor da variável b: {b}", end="\n\n")
+
+localizacao = ['Missão Velha', 'Ceará', 'Brazil']
+cidade, estado, pais = localizacao
+print(f"Lista original: {localizacao}")
+print(f"O valor da variável cidade: {cidade}")
+print(f"O valor da variável estado: {estado}")
+print(f"O valor da variável pais: {pais}", end="\n\n")
+
+
