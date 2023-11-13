0d2ea0f08dbb2ca7efdeb987f6f84b1074b0c59a
uelionobre <uelionobre@hotmail.com>
2023-11-13 11:39:32 -0300

    Arquivo com códigos de exemplos para saída de dados

---
+++ 01-saida-de-dados.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+  print("ENTRADA DE DADOS")
+  user_input = input("Digite uma mensagem: ")
+  print(f"O valor digitado foi: {user_input}")
+  
+  print("\n" * 2)
+  print("SAÍDA DE DADOS")
+  print("concatenando", "valores", "em", 2023, True, False)
+  print("")
+
