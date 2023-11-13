0e0bb5369e676d62a11197be44972484b29d5634
uelionobre <uelionobre@hotmail.com>
2023-11-13 11:48:16 -0300

    Comandos para saída de dados no terminal

--- 01-saida-de-dados.py
+++ 01-saida-de-dados.py
@@ -1,12 +1,22 @@
 import sys
 
 if __name__ == "__main__":
-  print("ENTRADA DE DADOS")
-  user_input = input("Digite uma mensagem: ")
-  print(f"O valor digitado foi: {user_input}")
-  
-  print("\n" * 2)
   print("SAÍDA DE DADOS")
-  print("concatenando", "valores", "em", 2023, True, False)
-  print("")
+  print("concatenando", "valores", "em", 2023, True, "com a função print")
+  
+  print("\n")
+  print("Usando separador de parametro para impressao")
+  print("Separando", "algumas", "palavras", True, 2023, sep=" - ")
+  
+  print("\n")
+  print("Usando flag de fim de linha")
+  print("Imprimindo", end=" ")
+  print("Uma linha inteira", end=" ")
+  print("De forma separada")
+  
+  print("\n")
+  print("Imprimindo erros na saída do terminal")
+  print(f"Imprimindo um erro em stderr", file=sys.stderr)
+  print(f"Imprimindo uma mensagem em stdout", file=sys.stdout)
+  
 
