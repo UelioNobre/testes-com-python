2b316a1fa8e6497d01d39c560c9b921f2adcbda3
uelionobre <uelionobre@hotmail.com>
2023-11-14 20:56:40 -0300

    Gera manipula uma excecao para que a apliacao nao pare

--- 03-manipula-excecao.py
+++ 03-manipula-excecao.py
@@ -4,10 +4,14 @@ notas_carregadas = []
 while len(notas_carregadas) < base_media:
   nota = input(f"Insira o valor da nota {len(notas_carregadas) + 1}: ")
   
-  if nota.isdigit() == False:
+  try:
+    if nota.isdigit() == False:
       raise ValueError(f":: ERROR! O valor da nota ({nota}) é inválido!")
-  else:
-    notas_carregadas.append(int(nota))
+    else:
+      notas_carregadas.append(int(nota))
+
+  except ValueError as error_message:
+    print(error_message)
 
 media = sum(notas_carregadas) / base_media
 print(f"Media das {base_media} notas {notas_carregadas}: ", media)
