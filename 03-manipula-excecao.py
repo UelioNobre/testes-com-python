cd004f95a574b476809e640752abe42ac7532f66
uelionobre <uelionobre@hotmail.com>
2023-11-14 20:54:14 -0300

    Converte cada nota para um valor do tipo inteiro

--- 03-manipula-excecao.py
+++ 03-manipula-excecao.py
@@ -3,7 +3,7 @@ notas_carregadas = []
 
 while len(notas_carregadas) < base_media:
   nota = input(f"Insira o valor da nota {len(notas_carregadas) + 1}: ")
-  notas_carregadas.append(nota)
+  notas_carregadas.append(int(nota))
 
 media = sum(notas_carregadas) / base_media
 print(f"Media das {base_media} notas {notas_carregadas}: ", media)
