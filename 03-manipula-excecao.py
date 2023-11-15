fc5b9add6744a3d94b65bc7a6d5aa7df8612de77
uelionobre <uelionobre@hotmail.com>
2023-11-14 21:00:30 -0300

    Adiciona mensagem para cada execução feita com sucesso!

--- 03-manipula-excecao.py
+++ 03-manipula-excecao.py
@@ -11,7 +11,12 @@ while len(notas_carregadas) < base_media:
       notas_carregadas.append(int(nota))
 
   except ValueError as error_message:
-    print(error_message)
+    print(error_message, end="\n" * 2)
+  
+  else:
+    print(f"Nota {len(notas_carregadas)} carregada com sucesso!", end="\n" * 2)
 
 media = sum(notas_carregadas) / base_media
+
 print(f"Media das {base_media} notas {notas_carregadas}: ", media)
+print("\n"*2)
