cf663cf119cd3fc8c0979db42a3c357c8d07b4cc
uelionobre <uelionobre@hotmail.com>
2023-11-14 21:48:26 -0300

    Implementa funcao para carregar arquivo das notas

--- 03-exercicio-leitura-notas.py
+++ 03-exercicio-leitura-notas.py
@@ -2,6 +2,18 @@ caminho_arquivo_inexistente = '03-arquivo-inexistente.txt'
 caminho_arquivo_existente = '03-exercicio-leitura-notas.txt'
 caminho_arquivo_pessoas_reprovadas = '03-exercicio-pessoas_reprovadas.txt'
 
+def carregar_arquivos_notas(caminho_arquivo):
+  pessoas = []
+  
+  try:
+    with open(caminho_arquivo) as conteudo:
+      pessoas = conteudo.readlines()
+  except FileNotFoundError:
+    print(f":: ERROR :: Não foi possível encontrar o arquivo: {caminho_arquivo}")
+    return None
+  
+  return pessoas
+
 def processar_pessoas_reprovadas(caminho_arquivo):
   try:
     notas1 = carregar_arquivos_notas(caminho_arquivo)
