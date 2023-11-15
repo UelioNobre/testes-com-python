dc328a65ec81337d14c3e46e75acb420c452da4d
uelionobre <uelionobre@hotmail.com>
2023-11-14 21:47:10 -0300

    Exercicio para filtrar arquivo .txt em busca de pessoas reprovadas

---
+++ 03-exercicio-leitura-notas.py
@@ -0,0 +1,5 @@
+caminho_arquivo_inexistente = '03-arquivo-inexistente.txt'
+caminho_arquivo_existente = '03-exercicio-leitura-notas.txt'
+caminho_arquivo_pessoas_reprovadas = '03-exercicio-pessoas_reprovadas.txt'
+
+print("Fim!", end="\n"*2)
