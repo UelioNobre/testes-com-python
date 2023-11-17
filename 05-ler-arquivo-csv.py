db2a310d70a06d5b41d6c79ec8fe5e7c4ff98553
uelionobre <uelionobre@hotmail.com>
2023-11-17 09:45:53 -0300

    Criar relatorio em csv por departamento

--- 05-ler-arquivo-csv.py
+++ 05-ler-arquivo-csv.py
@@ -1,22 +1,15 @@
 import csv
 from rich import print
-from manipuladores_csv import read_csv_file
+from manipuladores_csv import read_csv_file, write_csv_file_hard_mode, department_filter
 
 
 caminho_arquivo_csv = 'graduacao_unb.csv'
 header, conteudo = read_csv_file(caminho_arquivo_csv)
-print(header)
 
-group_by_department = {}
-for row in conteudo:
-  department = row[15]
-  
-  if department not in group_by_department:
-    group_by_department[department] = 0
-  
-  group_by_department[department] += 1
-  
-print(department)
-print(group_by_department)
+cabecalho = ['Departamento', 'Total de Cursos']
+group_by_department = department_filter(conteudo)
+
+write_csv_file_hard_mode("department_report.csv", cabecalho, group_by_department)
+
 
 print('FIM')
