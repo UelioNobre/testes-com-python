9f6d948430344cc4897b2f8990295ba75a24c36b
uelionobre <uelionobre@hotmail.com>
2023-11-16 21:44:01 -0300

    Lista todos os departamentos

--- 05-ler-arquivo-csv.py
+++ 05-ler-arquivo-csv.py
@@ -1,9 +1,22 @@
+import csv
 from rich import print
 from manipuladores_csv import read_csv_file
 
 
 caminho_arquivo_csv = 'graduacao_unb.csv'
-conteudo = read_csv_file(caminho_arquivo_csv)
+header, conteudo = read_csv_file(caminho_arquivo_csv)
+print(header)
 
-print(conteudo)
+group_by_department = {}
+for row in conteudo:
+  department = row[15]
+  
+  if department not in group_by_department:
+    group_by_department[department] = 0
+  
+  group_by_department[department] += 1
+  
+print(department)
+print(group_by_department)
+
 print('FIM')
--- manipuladores_csv.py
+++ manipuladores_csv.py
@@ -1,6 +1,11 @@
+import csv
+
 def read_csv_file(caminho_arquivo):
-  with open(caminho_arquivo, mode='r') as ponteiro:
-    dados = ponteiro.read()
+  with open(caminho_arquivo, encoding="utf8") as ponteiro:
+    dados = csv.reader(ponteiro, delimiter=",", quotechar='"')
   
-  return dados
+    header, *conteudo = dados
+  
+  return (header, conteudo)
+
 
