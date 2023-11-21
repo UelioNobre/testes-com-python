7492c98542b7fc6997e2f850f9b3253f2a65824b
uelionobre <uelionobre@hotmail.com>
2023-11-21 20:29:09 -0300

    Utilizando fixture nos testes

--- 02-automatizando-o-bug-hunting-com-pytest/a_test.py
+++ 02-automatizando-o-bug-hunting-com-pytest/a_test.py
@@ -1,3 +1,17 @@
+import pytest
+
+"""Usando Fixture
+
+Ao criar um metodo fixture, ele deve ser passado 
+com parametro, para a função de teste
+"""
+
+
+@pytest.fixture
+def my_list():
+    return [1, 2, 3]
+
+
 def test_um_teste_simples():
     assert True
 
@@ -5,3 +19,13 @@ def test_um_teste_simples():
 def test_multiplica_item_da_lista():
     minha_lista = [1, 2, 3]
     assert [item * 3 for item in minha_lista] == [3, 6, 9]
+
+
+"""
+Este teste utiliza uma função do Pytest que age 
+como "afterAll/beforeAll"
+"""
+
+
+def test_lista_com_multiplos_items(my_list):
+    assert [item * 3 for item in my_list] == [3, 6, 9]
