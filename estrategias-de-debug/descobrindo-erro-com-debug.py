b07cc4a63c809426271c4de2280b335a807674ed
uelionobre <uelionobre@hotmail.com>
2023-11-21 10:48:07 -0300

    Separa os valores que deram erros, do que foram processados com sucesso

--- estrategias-de-debug/descobrindo-erro-com-debug.py
+++ estrategias-de-debug/descobrindo-erro-com-debug.py
@@ -3,6 +3,7 @@ from math import factorial
 
 def map_factorial(numbers):
     result = []
+    imposible_results = []
 
     for num in numbers:
         try:
@@ -11,18 +12,21 @@ def map_factorial(numbers):
             print("Erro de tipo")
             print(f"Numero: {num}, Type: {type(num)}")
             print(f"Erro: {err}", end="\n" * 3)
+            imposible_results.append(num)
         except ValueError as err:
             print("Erro de valor")
             print(f"Numero: {num}, Type: {type(num)}")
             print(f"Erro: {err}", end="\n" * 3)
+            imposible_results.append(num)
 
-    return result
+    return [result, imposible_results]
 
 
 def main():
     input_list = ["um", 2, 3, 4, -5]
-    input_factorial = map_factorial(input_list)
-    print(input_factorial)
+    results, impossible_results = map_factorial(input_list)
+    print(f"Números calculados: {results}")
+    print(f"Valores não calculados: {impossible_results}")
 
 
 if __name__ == "__main__":
