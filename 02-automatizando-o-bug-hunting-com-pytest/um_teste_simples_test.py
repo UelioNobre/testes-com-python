745ede7dea86afea74c0a5f838e6a54b5e9cffc8
uelionobre <uelionobre@hotmail.com>
2023-11-22 13:21:04 -0300

    Separa arquivos para testar o arquivo conftest.py com uma fixture

---
+++ 02-automatizando-o-bug-hunting-com-pytest/um_teste_simples_test.py
@@ -0,0 +1,2 @@
+def test_um_teste_simples():
+    assert True
--- 02-automatizando-o-bug-hunting-com-pytest/usando_fixtures_test.py
+++ 02-automatizando-o-bug-hunting-com-pytest/usando_fixtures_test.py
@@ -1,14 +1,5 @@
-def test_um_teste_simples():
-    assert True
-
-
-def test_multiplica_item_da_lista():
-    minha_lista = [1, 2, 3]
-    assert [item * 3 for item in minha_lista] == [3, 6, 9]
-
-
 """
-Este teste utiliza uma função do Pytest que age
+Este teste utiliza uma função (pytest.fixture) do Pytest que age
 como "afterAll/beforeAll"
 """
 
