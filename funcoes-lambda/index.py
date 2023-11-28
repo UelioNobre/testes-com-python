0dfe36019d147026f65bfe4b22bfe67b3a21232f
uelionobre <uelionobre@hotmail.com>
2023-11-28 11:52:09 -0300

    adiciona uma funcao para exibir um extrato

--- funcoes-lambda/index.py
+++ funcoes-lambda/index.py
@@ -2,6 +2,14 @@ def calcular_comissao(valor, porcentagem):
     return valor * porcentagem
 
 
+def show_extrato_cli(salario, porcentagem, comisao, proventos_totais):
+    print(f"Valor do salário: R${salario}")
+    print(f"Porcentagem de acréscimo: {porcentagem}%")
+
+    print(f"Valor da comissão: R${comisao}")
+    print(f"Total de proventos: R${proventos_totais}", end="\n" * 2)
+
+
 salarios_e_comissoes = [
     [100, 0.10],
     [1000, 0.15],
@@ -9,12 +17,10 @@ salarios_e_comissoes = [
 
 for valores in salarios_e_comissoes:
     salario, porcentagem = valores
+    comissao = calcular_comissao(salario, porcentagem)
+    proventos_totais = salario + comissao
 
-    print(f"Valor do salário: R${salario}")
-    print(f"Porcentagem de acréscimo: {porcentagem}%")
+    show_extrato_cli(salario, porcentagem, comissao, proventos_totais)
 
-    valor_comissao = calcular_comissao(salario, porcentagem)
-    proventos_totais = salario + valor_comissao
 
-    print(f"Valor da comissão: R${valor_comissao}")
-    print(f"Total de proventos: R${proventos_totais}", end="\n" * 2)
+# usando lambda
