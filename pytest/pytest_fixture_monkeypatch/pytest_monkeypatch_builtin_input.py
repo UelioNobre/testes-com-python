6cdb7fd58cae44e2193b516f9c41fd7d00df1147
uelionobre <uelionobre@hotmail.com>
2023-12-04 01:56:01 -0300

    Testa o BIF "input()" do python

--- pytest/pytest_fixture_monkeypatch/pytest_monkeypatch_builtin_input.py
+++ pytest/pytest_fixture_monkeypatch/pytest_monkeypatch_builtin_input.py
@@ -1,5 +1,13 @@
-def show_message():
-    message = input("Informe uma mensagem: ")
+from funcoes_para_testar import show_message
 
-    print("A entrada informada foi: ", end="")
-    print(message)
+
+def mock_input_hello_world(_):
+    return "Hello world!"
+
+
+def test_show_message(monkeypatch):
+    # Simula o usuário digitando no terminal
+    monkeypatch.setattr("builtins.input", mock_input_hello_world)
+    result = show_message()
+
+    assert result == "A entrada informada foi: Hello world!"
