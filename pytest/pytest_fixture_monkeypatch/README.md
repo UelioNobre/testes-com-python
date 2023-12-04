4c1e96f3701f69856cfa0866b295e04f2d216181
uelionobre <uelionobre@hotmail.com>
2023-12-04 01:01:31 -0300

    Adiciona uma funcao simples para entrada de dados do usuário

---
+++ pytest/pytest_fixture_monkeypatch/README.md
@@ -0,0 +1,5 @@
+# Monkeypatch
+
+Um stub Python (ou é mais que isso?)
+
+[Documentação](https://docs.pytest.org/en/latest/how-to/monkeypatch.html)
---
+++ pytest/pytest_fixture_monkeypatch/pytest_monkeypatch_builtin_input.py
@@ -0,0 +1,5 @@
+def show_message():
+    message = input("Informe uma mensagem: ")
+
+    print("A entrada informada foi: ", end="")
+    print(message)
